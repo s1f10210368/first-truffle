1af11a013eb4078a537f92287874da4eefce1a10
s1f10210368 <s1f102103684@iniad.org>
2024-05-14 22:53:20 +0900

    R

---
+++ contracts/firtst-truffle.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.21;
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+ 
+contract TrufflePRJ is ERC20{
+   string public name = "TrufflePRJ";
+   string public symbol = "TRFPJ";
+   uint256 public decimals = 18;
+ 
+constructor(uint256 initialSupply) public {
+   _mint(msg.sender, initialSupply);
+   }
+}
